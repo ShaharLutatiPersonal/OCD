89e3e2aa6c0e51a5feb242b204dc7055c230101a
Bar <baryaacovi7@gmail.com>
2022-11-18 19:45:35 +0200

    added yolo test run

---
+++ run_yolo.py
@@ -0,0 +1,22 @@
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+imgs = ['https://ultralytics.com/images/zidane.jpg']  # batch of images
+
+# Inference
+results = model(imgs)
+
+# Results
+# results.print()
+results.save()  # or .show()
+
+results.xyxy[0]  # img1 predictions (tensor)
+results.pandas().xyxy[0]  # img1 predictions (pandas)
+#      xmin    ymin    xmax   ymax  confidence  class    name
+# 0  749.50   43.50  1148.0  704.5    0.874023      0  person
+# 1  433.50  433.50   517.5  714.5    0.687988     27     tie
+# 2  114.75  195.75  1095.0  708.0    0.624512      0  person
+# 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
