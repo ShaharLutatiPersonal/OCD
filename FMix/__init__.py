b04edb6fc77e968b79874f0272da54d841659043
BarY7 <baryaacovi7@gmail.com>
2022-11-21 21:02:39 +0200

    update init file of fmix

--- FMix/__init__.py
+++ FMix/__init__.py
@@ -1,2 +1,10 @@
 import sys
 sys.path.append("C:\\Users\\user\\Desktop\\University\\Masters\\Semester5\\OCD\\FMix")
+
+from analysis import *
+from datasets import *
+from experiments import *
+from implementations import *
+from models import *
+from notebooks import *
+from utils import *
